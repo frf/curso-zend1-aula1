6ef44ed5b535f1c8ca7e2c286db21e8ad4362416
Fabio Rocha <fabio@fabiofarias.com.br>
2014-01-09 16:01:49 -0200

    Update usuario.sql

--- docs/usuario.sql
+++ docs/usuario.sql
@@ -1,1 +1,9 @@
-CRETAE USSER
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+ 
